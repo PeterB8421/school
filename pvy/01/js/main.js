9e38583ddeb27c02bd90d81a58471abca307feef
vyt2-pc2 <vyt2.2@sspu-opava.cz>
2017-09-22 09:36:15 +0200

    funkce.js randGen

--- pvy/01/js/main.js
+++ pvy/01/js/main.js
@@ -1,3 +1,21 @@
 console.log('Script loaded!');
 console.log(document.getElementById("zahlavi"));
 document.getElementsByTagName('img')[0].width /= 2;
+//
+document.getElementById('nadpis').addEventListener('click', function(){
+    this.innerText = prompt('Napište něco: ');
+    this.style.color = 'red';
+});
+document.getElementsByTagName('img')[0].addEventListener('mouseenter', function(){
+    this.src = 'img/irma2.jpg';
+    this.style.border = 'solid 2px red';
+    this.style.opacity = 0.75;
+});
+document.getElementsByTagName('img')[0].addEventListener('mouseleave', function(){
+    this.src = 'img/irma1.jpg';
+    this.style.border = 'none';
+    this.style.opacity = 1;
+});
+document.getElementById('menic').addEventListener('click', function(){
+    document.getElementsByTagName('main')[0].style.backgroundColor = randGenRGB();
+});
