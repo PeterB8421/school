869a9a650a11458585dbc0e16087d17add4a0224
Petr Balok <petrbalok@gmail.com>
2017-11-08 17:43:27 +0100

    Oprava kódu stránky pvy/bmi/index.html

--- pvy/bmi/js/main.js
+++ pvy/bmi/js/main.js
@@ -3,5 +3,11 @@ document.getElementById('count').addEventListener('click', function(){
     var h = document.getElementById('height').value;
     var bmi = (m / Math.pow(h, 2)).toFixed(2);
     console.log(bmi);
+    if(bmi < 16.5 || bmi > 40){
+        document.getElementById('result').style.color = "red";
+    }
+    else{
+        document.getElementById('result').style.color = "black";
+    }
     document.getElementById('result').innerHTML = 'Vaše BMI: '+bmi;
 })
