9717cdf68b7b7555c1904a36a7bc8b92e53ae6e0
Petr Balok <petrbalok@gmail.com>
2018-02-09 09:32:37 +0100

    jQuery
    Pokrok v interaktivní stránce za pomoci jQuery.

---
+++ pvy/jquery/js/data.js
@@ -0,0 +1,92 @@
+var data = [
+    {
+        'name':'Tomáš Garrigue Masaryk',
+        'inoffice':'1918-1935',
+        'description':'Český vědec, filozof, pedagog, politik a žurnalista zakladatel novodobého československého státu, československý prezident',
+        'photo':'prezident9.jpg',
+        'borned':'7. 3. 1850 Hodonín',
+        'died':'14. 9. 1937 Lány',    
+        'link':'https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/tomas-garrigue-masaryk' 
+    },
+    {
+        'name':'Edvard Beneš',
+        'inoffice':'1935-1938, 1945-48',
+        'description':'Český a československý politik, diplomat, prezident 1935-48',
+        'photo':'prezident8.jpg',
+        'borned':'28. 5. 1884 Kožlany na Rakovnicku',
+        'died':'3. 9. 1948 Sezimovo Ústí',
+        'link':'https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/edvard-benes' 
+    },
+    {
+        'name':'Emil Hácha',
+        'inoffice':'1938-1945',
+        'description':'Český právník a státník, prezident Česko-Slovenské republiky a státní prezident Protektorátu Čechy a Morava',
+        'photo':'prezident5.jpg',
+        'borned':'12. 7. 1872 Trhové Sviny',
+        'died':'27. 6. 1945 Praha',
+        'link':'https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/emil-hacha' 
+    },
+    {
+        'name':'Klement Gottwald',
+        'inoffice':'1948-53',
+        'description':'Komunistický politik a československý prezident',
+        'photo':'prezident7.jpg',
+        'borned':'23. 11. 1896 Dědice',
+        'died':'14. 3. 1953 Praha',
+        'link':'https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/klement-gottwald' 
+    },
+    {
+        'name':'Antonín Zápotocký',
+        'inoffice':'1953-57',
+        'description':'Český sociálně demokratický a poté československý komunistický politik a prezident',
+        'photo':'prezident10.jpg',
+        'borned':'19. 12. 1884 Zákolany',
+        'died':'13. 11. 1957 Praha',
+        'link':'https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/antonin-zapotocky' 
+    },            
+    {
+        'name':'Antonín Novotný',
+        'inoffice':'1957-68',
+        'description':'Československý komunistický politik a prezident',
+        'photo':'prezident6.jpg',
+        'borned':'10. 12. 1904 Praha-Letňany',
+        'died':'28. 1. 1975 Praha',
+        'link':'https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/antonin-novotny' 
+    },            
+    {
+        'name':'Ludvík Svoboda',
+        'inoffice':'1968-75',
+        'description':'Československý armádní generál, komunistický politik a  prezident v letech 1968 - 1975',
+        'photo':'prezident4.jpg',
+        'borned':'25. 11. 1895 Hroznatín',
+        'died':'20. 9. 1979 Praha',
+        'link':'https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/ludvik-svoboda' 
+    },            
+    {
+        'name':'Gustáv Husák',
+        'inoffice':'1975-89',
+        'description':'Slovenský právník, československý komunistický politik a prezident',
+        'photo':'prezident3.jpg',
+        'borned':'10. 1. 1913 Bratislava-Dúbravka',
+        'died':'18. 11. 1991 Bratislava',
+        'link':'https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/gustav-husak' 
+    },
+    {
+        'name':'Miloš Zeman',
+        'inoffice':'2013 - současnost',
+        'description':'Český politik, ekonom a prognostik.',
+        'photo':'prezident11.jpg',
+        'borned':'28. 9. 1944 Kolín',
+        'died':'',
+        'link':'https://www.hrad.cz/cs/prezident-cr/soucasny-prezident-cr'
+    },
+    {
+        'name':'Václav Klaus',
+        'inoffice':'2003 - 2013',
+        'description':'Český ekonom, politik a spoluzakladatel Občanské demokratické strany.',
+        'photo':'prezident2.jpg',
+        'borned':'19. 6. 1941 Praha',
+        'died':'',
+        'link':'https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/vaclav-klaus'
+    }            
+];
--- pvy/jquery/js/main.js
+++ pvy/jquery/js/main.js
@@ -1,10 +1,24 @@
+function findJSONObject(data, attrib, value){
+    var result;
+    data.forEach(function(val, index){
+        if(val[attrib] == value){
+            result = val;
+        }
+    })
+    return result;
+}
+
 $(function(){
+    $("#tick").hide();
     var obj = null;
+    var colors = ["darkred","darkblue","white"];
+    var chosenColor = colors[Math.floor(Math.random()*colors.length)];
     $(".row:first figcaption").hide();
     $(".row:first figure").on('mouseover', function(){
         $(this).find("figcaption").toggle(100);
     })
-    $("header.jumbotron").css({"background-color":"#003366", "color":"white"});
+    $("#tick").css({'position':'absolute'});
+    $("header.jumbotron").css({"background-color":chosenColor, "color":"#888"});
     $("h2:contains('1948 - 1989')").append(" <i>(Komunisti)</i>");
     $(".well").on('click', function(){
         $(this).nextAll().toggle(500);
@@ -25,21 +39,43 @@ $(function(){
             $("body").css({"background-color":"white"});
         }
     })
-    $(".row:nth-child(2) figure").on('click', function(){
-        if(obj == null){
-            obj = $(this);
+    $('figure').on('click', function (event) {
+        if(event.ctrlKey){
+            var prezident = $(this).find('figcaption').text();
+            var person = findJSONObject(data, "name", prezident);
+            if(person != undefined){
+                $("#window").modal('show');
+                $("h4.modal-title").text(person.name +' ('+ person.inoffice + ')');
+                $(".modal-body").html('<img src="fotky/'+ person.photo +'"><br>');
+                $(".modal-body").append('<b>Narozen</b>: '+ person.borned +'<br>');
+                if(person.died.length > 0){
+                    $(".modal-body").append('<b>Zemřel</b>: '+ person.died +'<br>');
+                }
+                $(".modal-body").append('<b>Popis</b>: '+ person.description +'<br>');
+                $(".modal-body").append("<hr><a href='"+ person.link +"' class='btn btn-primary' target='_blank'>Podrobnosti</a>")
+                $(".modal-header").css({"background-color":"#00a","color":"white"});
+                $(".modal-header").css("text-align","center");
+            }
         }
-        else{
-            var store = $(this).html();
-            $(this).html(obj.html());
-            $(obj).html(store);
-            obj = null;
+        if(event.shiftKey){
+            if(obj == null){
+                obj = $(this);
+            }
+            else{
+                var store = $(this).html();
+                $(this).html(obj.html());
+                $(obj).html(store);
+                obj = null;
+            }
         }
-    })
-    $('figure').on('click', function () {
-        $("#window").modal('show');
-        $("h4.modal-title").text($(this).find('figcaption').text());
-        $(".modal-header").css({"background-color":"#00a","color":"white"});
-        $(".modal-header").css("text-align","center");
       })
 });
+var element;
+$("body").on("mouseenter","img", function(event){
+    var pos = $(this).offset();
+    element = $(this);
+    $("#tick").css({'top':pos.top,'left':pos.left}).show().on('click', function(){
+        element.parent().parent().remove();
+        $(this).hide();
+    });
+})
