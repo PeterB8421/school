609bc7d761dbf774ca7438f7f21ded6b51687f81
vyt2-pc2 <vyt2.2@sspu-opava.cz>
2018-03-16 09:30:19 +0100

    jQuery UI

---
+++ pvy/jqueryui/data/data.js
@@ -0,0 +1,109 @@
+var data = 
+[
+    {
+       "zanr":"fantasy",
+       "knihy":[
+          {
+             "id":1,              
+             "titul":"Hra o trůny",
+             "autor":"George Raymond Richard Martin",
+             "obsah":"Úvodní část epického fantasy eposu odehrávajícího se ve světě temného středověku, mezi statečnými rytíři, mocnými čaroději, draky, zlovlky a čarostromy...",
+             "vydani":"2011",
+             "stran":848,
+             "cena":350,
+             "obalka":"hra-o-truny.jpg"
+          },
+          {
+             "id":2,              
+             "titul":"Zaklínač 1 - Poslední přání",
+             "autor":"Andrzej Sapkowski",
+             "obsah":"Ošlehaný muž bez věku, jehož bílé vlasy nejsou znakem stáří, ale mutace, kterou musel podstoupit. Placený i dobrovolný likvidátor prapodivných tvorů: mantichor, trollů, vidlohonů, strig, amfisbain - pokud ovšem ohrožují lidský rod; v takovém případě zabíjí i bytost zvanou člověk. Prvotřídní, skvělý bojovník, který není neporazitelný ani nezranitelný - naopak, téměř z každého dobrodružství si odnáší další šrámy na těle i na duši.",
+             "vydani":"2009",
+             "stran":280,
+             "cena":225,
+             "obalka":"posledni-prani.jpg"
+          },
+          {
+             "id":3,              
+             "titul":"Harry Potter a Kámen mudrců",
+             "autor":"Joanne Kathleen Rowling",
+             "obsah":"Brýlatý chlapec s nachovou jizvou na čele, připomínající blesk – to je Harry Potter. Jedenáctiletého učedníka čarodějných umění stvořila nadaná spisovatelka J. K. Rowlingová a ze své ostrovní britské vlasti jej vyslala pro potěchu všem duším okouzleným fantazií.",
+             "vydani":"2000",
+             "stran":288,
+             "cena":260,
+             "obalka":"harry-potter-a-kamen-mudrcu.jpg"
+          }
+       ]
+    },
+    {
+       "zanr":"detektivka",
+       "knihy":[
+          {
+             "id":4,              
+             "titul":"Sněhulák",
+             "autor":"Jo Nesbø",
+             "obsah":"Sněhulák je sedmou knihou volné série krimirománů, jejichž hrdinou je vyšetřovatel Harry Hole.",
+             "vydani":"2012",
+             "stran":510,
+             "cena":399,
+             "obalka":"snehulak.png"
+          },
+          {
+             "id":5,              
+             "titul":"Skála",
+             "autor":"Peter May",
+             "obsah":"Skotský ostrov Lewis je krásné, ale drsné odlehlé místo bez stromů, bičované větry a pokryté vřesovišti. Topí se tam rašelinou a mluví gaelsky. A snad jen strach lidí z Boha je tam silnější než starost o živobytí. Pod zástěrkou víry však přežívají staré pohanské hodnoty a prapůvodní touha po krvi a pomstě. Na ostrov přijíždí nedávnou ztrátou syna poznamenaný policejní detektiv Fin Macleod, jenž se má podílet na vyšetřování brutální vraždy, spáchané podle stejného scénáře jako vražda, jíž se zabývá v Edinburghu.",
+             "vydani":"2013",
+             "stran":392,
+             "cena":299,
+             "obalka":"skala.jpg"
+          },
+          {
+             "id":6,              
+             "titul":"Deset malých černoušků",
+             "autor":"Agatha Christie",
+             "obsah":"Nejprodávanější detektivka všech dob, nyní rovněž ve vynikajícím britském seriálovém zpracování pod názvem Až tam nezbyl žádný.",
+             "vydani":"2017",
+             "stran":232,
+             "cena":215,
+             "obalka":"deset-malych-cernousku.jpg"
+          }
+       ]
+    },
+    {
+       "zanr":"historie",
+       "knihy":[
+          {
+             "id":7,              
+             "titul":"Egypťan Sinuhet",
+             "autor":"Mika Waltari",
+             "obsah":"Sinuhet je dítě neznámého původu. Připlul ke své nevlastní matce Kipě a otci Senmutovi v proutěném košíku po řece. Učil se v chrámě Amonově a stal se jako otec lékařem. Dobrodružství, která zažil, jsou četná. A tím nejpovolanějším, kdo by nám o nich měl povědět, je on sám.",
+             "vydani":"2004",
+             "stran":832,
+             "cena":420,
+             "obalka":"egyptan-sinuhet.jpg"
+          },
+          {
+             "id":8,              
+             "titul":"Ranhojič",
+             "autor":"Noah Gordon",
+             "obsah":"Rozsáhlý román se odehrává v 11. století. Líčí bouřlivé drama nadaného léčitele, který bojoval s nemocemi a se smrtí v primitivních podmínkách středověké Anglie a Persie, kde poznal smyslnou nádheru zlatého věku arabské a židovské civilizace...",
+             "vydani":"2004",
+             "stran":696,
+             "cena":431,
+             "obalka":"ranhojic.jpg"
+          },
+          {
+             "id":9,              
+             "titul":"Marketa Lazarová",
+             "autor":"Vladislav Vančura",
+             "obsah":"Klasický román Vladislava Vančury přináší příběh o čisté duši a čisté lásce uprostřed chladného a nemilosrdného středověku. Markéta Lazarová má být zaslíbena bohu a strávit život v bezpečí kláštera, ale namísto toho je unesena čeládkou loupežníka Kozlíka. Uprostřed nelítostné sebranky kupodivu nenachází jen ponížení, ale i lásku, pokoru a pokřivený, leč upřímný smysl pro čest. Poprvé román vyšel v roce 1931 a v roce 1967 na jeho motivy František Vláčil natočil film, který se zařadil k vrcholům české kinematografie.",
+             "vydani":"2008",
+             "stran":132,
+             "cena":105,
+             "obalka":"marketa-lazarova.jpg"
+          }
+       ]
+    }
+ ]
+ 
